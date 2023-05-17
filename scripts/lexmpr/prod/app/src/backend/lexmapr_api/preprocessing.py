ee47031f1ab3456564a59e8546532bb97d1953a7
razzors1998@gmail.com <razzors1998@gmail.com>
2023-05-18 00:14:59 +0200

    linking pair of entities

--- scripts/lexmpr/prod/app/src/backend/lexmapr_api/preprocessing.py
+++ scripts/lexmpr/prod/app/src/backend/lexmapr_api/preprocessing.py
@@ -1,5 +1,6 @@
 import io
 import json
+from itertools import pairwise
 from typing import List, Dict
 import pandas as pd
 
@@ -20,7 +21,9 @@ class Preprocessing:
 
     def run(self):
         df = self.parse_input(self.input)
-        entities = self.merge_important_entities(df["ingredients_entities"])
+        entities = self.keep_important_entities(df["ingredients_entities"])
+        entities = self.pair_merge_entities(entities)
+        entities = self.entities_to_string(entities)
         csv_text = self.convert_entities_to_lexmapr_input(entities)
         return df, csv_text
 
@@ -48,27 +51,70 @@ class Preprocessing:
         return df
 
     @staticmethod
-    def merge_important_entities(recipes: pd.Series) -> pd.Series:
+    def keep_important_entities(recipes: pd.Series) -> pd.Series:
         """
-        Connect entities into string if not QUANTITY or UNIT entity type
+        Keep entities if not **QUANTITY** or **UNIT** entity type
 
         Args:
-            recipes (pd.Series): text with QUANTITY and UNITS entities
+            recipes (pd.Series): list of entities with QUANTITY and UNITS entities per recipe
 
         Returns:
-            pd.Series: text without QUANTITY and UNIT entities
+            pd.Series: list of entities without QUANTITY and UNIT entities per recipe
         """
 
         def for_each_row(row: List[Dict]):
-            text = [
-                ent["entity"]
-                for ent in row
-                if ent["type"] not in ["QUANTITY", "UNIT"]
-            ]
-            return text
+            important_entities = list(filter(lambda ent: ent["type"] not in ["QUANTITY", "UNIT"], row))
+            return important_entities
 
-        merged_entities = recipes.apply(for_each_row)
-        return merged_entities
+        results = recipes.apply(for_each_row)
+        return results
+
+    @staticmethod
+    def pair_merge_entities(recipes: pd.Series) -> pd.Series:
+        """
+        Merge entities when first entity type is **COLOR, PHYSICAL_QUALITY, PROCESS** and second is **FOOD**
+
+        Args:
+            recipes (pd.Series): list of entities per recipe
+
+        Returns:
+            pd.Series: list of entities with merged entities format per recipe
+        """
+
+        def for_each_row(row: List[Dict]):
+            connected_entities = [
+                {
+                    **e2,
+                    'entity': f"{e1['entity']} {e2['entity']}"
+                }
+                if e1['type'] in ['COLOR', 'PHYSICAL_QUALITY', 'PROCESS'] and e2['type'] is 'FOOD'
+                else
+                e1
+                for e1, e2 in pairwise(row)
+            ]
+            return connected_entities
+
+        results = recipes.apply(for_each_row)
+        return results
+
+    @staticmethod
+    def entities_to_string(recipes: pd.Series) -> pd.Series:
+        """
+        Get entities string representation
+
+        Args:
+            recipes (pd.Series): list of entities per recipe
+
+        Returns:
+            pd.Series: list of entities in string format per recipe
+        """
+
+        def for_each_row(row: List[Dict]):
+            text_entities = [e['entity'] for e in row]
+            return text_entities
+
+        results = recipes.apply(for_each_row)
+        return results
 
     @staticmethod
     def convert_entities_to_lexmapr_input(recipes: pd.Series) -> pd.Series:
